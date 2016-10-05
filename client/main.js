f872630d136a5209dbf9e4806dc8f70ac5474af2
Ivan Tarozzi <itarozzi@gmail.com>
2016-10-05 14:40:56 +0200

    add list of components from Collection

--- client/main.js
+++ client/main.js
@@ -3,20 +3,22 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Template.componentsList.helpers({
+  create: function(){
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
   },
-});
+  rendered: function(){
+  },
+  destroyed: function(){
+  },
+  componentsNr: function() {
+    return 11;
+  },
+  components() {
+    return ComponentsCollection.find({});
+  },
+  componentsCount() {
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    return ComponentsCollection.find({}).count();
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function () {
+  if (ComponentsCollection.find().count() === 0) {
+    var dummycomponents = [{
+      category: 'Transistor',
+      code: 'BC857',
+      description: 'a test transistor'
+    },
+    {
+      category: 'Microcontroller',
+      code: 'AT Tiny81',
+      description: 'a test Microcontroller'
+
+    }];
+
+    while (dummycomponents.length > 0) {
+      ComponentsCollection.insert(dummycomponents.pop());
+    }
+    console.log('Added fixtures');
+  }
 });
