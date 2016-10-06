dcd472e42c82908ec5d3c958190a4ed7a2f49fc2
Ivan Tarozzi <itarozzi@gmail.com>
2016-10-06 18:07:27 +0200

    add component detail.... partial, not working

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Session.setDefault('selectedComponentDetailId', '');
+
 Template.componentsList.helpers({
   create: function(){
 
@@ -18,7 +20,35 @@ Template.componentsList.helpers({
     return ComponentsCollection.find({});
   },
   componentsCount() {
-
     return ComponentsCollection.find({}).count();
+  }
+});
+
+Template.componentsList.events({
+  "click .table-row": function(event, template){
+    let  selectedId = '';
+
+    if (typeof selectedId === 'string' || selectedId instanceof String) {
+      selectedId = "s" + event.currentTarget.id;
+
+    } else {
+      selectedId = event.currentTarget.id._id;
+    }
+
+    console.log("ROW Click" + selectedId);
+    Session.set('selectedComponentDetailId', selectedId);
+  }
+});
+
+
+Template.componentsRow.helpers({
+  isDetailVisible() {
+
+    const currentId = this._id;
+    const selId = Session.get('selectedComponentDetailId');
+
+    
+
+    return selId === currentId ? true : false;
   }
 });
